3274fe9226a72c12e75284764180288dcfaa7ce4
todorovicdunja <153907234+todorovicdunja@users.noreply.github.com>
2024-06-04 18:46:29 +0200

    Add files via upload

---
+++ Susedne drzave/script.js
@@ -0,0 +1,65 @@
+function openWindow(id) {
+    switch (id) {
+      case 'albanija':
+        window.open('./drzave/' + id + '.html',
+            'Albanija',
+            'width=450px,height=300px');
+        break;
+      case 'bosna':
+        window.open(
+            './drzave/' + id + '.html',
+            'Bosna',
+            'width=450px,height=300px'
+        );
+        break;
+      case 'bugarska':
+        window.open(
+          './drzave/' + id + '.html',
+            'Bugarska',
+            'width=450px,height=300px'
+        );
+        break;
+      case 'crna_gora':
+        window.open(
+          './drzave/' + id + '.html',
+            'Crna Gora',
+            'width=450px,height=300px'
+        );
+        break;
+      case 'hrvatska':
+        window.open(
+          './drzave/' + id + '.html',
+            'Hrvatska',
+            'width=450px,height=300px'
+        );
+        break;
+        case 'madjarska':
+        window.open(
+          './drzave/' + id + '.html',
+            'Madjarska',
+            'width=450px,height=300px'
+        );
+        break;
+        case 'srbija':
+        window.open(
+          './drzave/' + id + '.html',
+            'Srbija',
+            'width=450px,height=300px'
+        );
+        break;
+        case 'makedonija':
+        window.open(
+          './drzave/' + id + '.html',
+            'Makedonija',
+            'width=450px,height=300px'
+        );
+        break;
+        case 'rumunija':
+        window.open(
+          './drzave/' + id + '.html',
+            'Rumunija',
+            'width=450px,height=300px'
+        );
+        break;
+    }
+  }
